a7cc703785091bc7751b5144982cc523c78f00d7
agungpangestu <agung_pangestu83@ymail.com>
2017-12-05 17:50:34 +0700

    Add Mobil table and mvc

---
+++ models/mobil.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Mobil = sequelize.define('Mobil', {
+    name: DataTypes.STRING,
+    min_bid: DataTypes.INTEGER,
+    max_bid: DataTypes.INTEGER,
+    time: DataTypes.INTEGER,
+    status: DataTypes.BOOLEAN
+  }
+  Mobil.associate = function (models) {
+    
+  };
+  return Mobil;
+};
