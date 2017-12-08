c95c83e3a17e42b83e7a07a8368beb2d30e638b4
agungpangestu <agung_pangestu83@ymail.com>
2017-12-08 08:33:02 +0700

    MVP

--- helpers/cekWinner.js
+++ helpers/cekWinner.js
@@ -1,30 +1,50 @@
+const Models = require('../models');
 function cekWinner() {
   Models.Mobil.findAll({include :[Models.bidding],order: [ [ Models.bidding, 'bid' ,'DESC'] ]}).then(dataBidding=>{
-    res.send(dataBidding)
+    // res.send(dataBid
     let tempBid = 0
     let tempUserId = null
     dataBidding.forEach(data => {
       if (!dataBidding.status && data.biddings.length == 1) {
         let updateData = {
+          statusWin : true,
           alreadyEmail :false
         }
-        Models.Mobil
+        Models.bidding
         .update(updateData,
-          {where : {id : data.biddings.id}})
+          {where : {id : data.biddings[0].id}})
         .then((result)=>{
         })
-        // data.biddings[0].alreadyEmail = true
       }
-      // else if (data.biddings.length > 1) {
-      //     if (biddings.bid > tempBid) {
-      //       tempBid = biddings.bid
-      //       tempUserId = biddings.UserId
-      //     }
-      //   Models.User.findById(tempUserId).then(user=>{
-      //     
-      //   })
-      // }
-        
+      else if (!dataBidding.status && data.biddings.length > 1) {
+        for (let i = 0; i < data.biddings.length; i++) {
+          if (i == 0 && data.biddings[i].statusWin == null) {
+            let id = data.biddings[i].id
+            Models.bidding
+            .update({
+              statusWin : true,
+              alreadyEmail :false
+            },
+              {where : {id : id}})
+              .then((result)=>{
+            })
+          }else if (i != 0 && data.biddings[i].statusWin == null) {
+            let id = data.biddings[i].id
+            Models.bidding
+            .update({
+              statusWin : false
+            },
+              {where : {id : id}})
+              .then((result)=>{
+            })
+          }
+        }
+        // 
+        // Models.User.findById(tempUserId).then(user=>{
+        //   
+        // })
+      }
+      
     });
     
   })
---
+++ helpers/chekerEmail.js
@@ -0,0 +1,49 @@
+const Models = require('../models');
+const Mail = require('../helpers/mail')
+
+function cekEmail() {
+  Models.Mobil.findAll({include :[Models.bidding],order: [ [ Models.bidding, 'bid' ,'DESC'] ]}).then(dataBidding=>{
+    res.send(dataBidding)
+    let tempBid = 0
+    let tempUserId = null
+    dataBidding.forEach(data => {
+      if (data.bidding>0) {
+        for (let i = 0; i < data.length; i++) {
+          if (!dataBidding.status && data.biddings[i].statusWin) {
+            let updateData = {
+              alreadyEmail :true
+            }
+            Models.bidding
+            .findById(data.biddings[i].UserId)
+            .then((err,result)=>{
+              Mail(result.email)
+            })
+          }
+          // else if (!dataBidding.status && data.biddings[i].statusWin) {
+          //   let updateData = {
+          //     alreadyWin : true,
+          //     alreadyEmail :false
+          //   }
+          //   data.biddings.forEach(data=>{
+          //     
+          //   })
+          //   let id = data.biddings[0].id
+          //   Models.bidding
+          //   .update(updateData,
+          //     {where : {id : id}})
+          //     .then((result)=>{
+          //     })
+          //     Models.User.findById(tempUserId).then(user=>{
+          //       
+          //   })
+          // }
+        }
+        
+      }
+      
+    });
+    
+  })
+}
+
+module.exports = cekEmail;
