3dafd2587e7c3f1cd29353b74199dd03473074a4
agungpangestu <agung_pangestu83@ymail.com>
2017-12-08 10:12:02 +0700

    fix email

--- helpers/chekerEmail.js
+++ helpers/chekerEmail.js
@@ -2,57 +2,45 @@ const Models = require('../models');
 const Mail = require('../helpers/mail')
 
 function cekEmail() {
-  Models.Mobil.findAll({include :[Models.bidding],order: [ [ Models.bidding, 'bid' ,'DESC'] ]}).then(dataBidding=>{
-
+  Models.bidding.findAll({order: [ [ 'bid' ,'ASC'] ]}).then(dataBidding=>{
+    
     let tempBid = 0
     let tempUserId = null
     dataBidding.forEach(data => {
-      if (data.bidding>0) {
-        for (let i = 0; i < data.bidding.length; i++) {
-          if (!dataBidding.status && data.biddings[i].statusWin) {
-            let updateData = {
-              alreadyEmail :true
-            }
-            Models.User
-            .findById(data.biddings[i].UserId)
-            .then((err,result)=>{
-              Mail(result.email)
-            })
-            Models.Mobil
-            .findById(data.biddings[i].Mobil.Id)
-            .then((err,mobil)=>{
-              Models.User
-              .findById(mobil.UserId)
-              .then((err,user)=>{
-                Mail(user.email)
-              })
-            })
+      Models.Mobil.findById(data.MobilId).then((err,row)=>{
+        if (!row.status && data.statusWin == true && data.alreadyEmail == false) {
+          let updateData = {
+            alreadyEmail :true
           }
-          // else if (!dataBidding.status && data.biddings[i].statusWin) {
-          //   let updateData = {
-          //     alreadyWin : true,
-          //     alreadyEmail :false
-          //   }
-          //   data.biddings.forEach(data=>{
-          //     
-          //   })
-          //   let id = data.biddings[0].id
-          //   Models.bidding
-          //   .update(updateData,
-          //     {where : {id : id}})
-          //     .then((result)=>{
-          //     })
-          //     Models.User.findById(tempUserId).then(user=>{
-          //       
-          //   })
-          // }
+          let id = data.id
+          Models.User
+          .findById(data.UserId)
+          .then((err,result)=>{
+            Mail(result.email)
+          })
+          Models.Mobil
+          .findById(data.MobilId)
+          .then((err,mobil)=>{
+            Models.User
+            .findById(mobil.UserId)
+            .then((err,user)=>{
+              Mail(user.email)
+            })
+          })
+          Models.bidding
+          .update(updateData,
+            {where : {id : id}})
+            .then((result)=>{
+            })
+      
         }
-        
-      }
+      })
       
     });
     
   })
 }
+
+cekEmail()
 
 module.exports = cekEmail;
